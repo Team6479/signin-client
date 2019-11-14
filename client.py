0c8c91053e96bcb2e97d23569cc9016513c30935
Leo Wilson <leo@leomwilson.com>
2019-11-13 19:09:08 -0700

    Everything works with a dummy backend now

--- client.py
+++ client.py
@@ -1,1 +1,38 @@
 import backend
+import time
+import os
+
+# True if the user is signing in, false if signing out
+def sign_in_out(id: int) -> bool:
+    # We only store times as ints, but this could be changed easily if more precision is required
+    timestamp: int = round(time.mktime(time.localtime()))
+    try: # If a sessionfile exists, the user is signed in and must be signed out
+        with open('data/sessions/' + str(id), 'r') as sessionfile:
+            backend.submit(id, int(sessionfile.read()), timestamp)
+        os.remove('data/sessions/' + str(id))
+        return False
+    except: # Create a new sessionfile
+        with open('data/sessions/' + str(id), 'w+') as sessionfile:
+            sessionfile.write(str(timestamp))
+        return True
+
+while True:
+    cmd: str = input('Please scan your ID card or enter your student number: ')
+    if cmd == '..':
+        # TODO: sign out all users and force the queue to push
+        break
+    else: # the number should be handled as an ID
+        try:
+            id: int = int(cmd)
+            name = backend.get_name(id)
+            if name:
+                if sign_in_out(id):
+                    print('Welcome, ' + name + '.')
+                else:
+                    print('Goodbye, ' + name + '.')
+            else:
+                name = backend.create_user(id, input('This appears to be your first time signing in. Please enter your name: '))
+                sign_in_out(id)
+                print('Welcome, ' + name + '.')
+        except:
+            print('Invalid ID or command.')
--- dummy_backend.py
+++ dummy_backend.py
@@ -1,1 +1,17 @@
 # This file follows the structure of backend.py and thus should be a drop-in replacement
+
+def get_name(id: int) -> str:
+    print(id)
+    try:
+        with open('data/users/' + str(id), 'r') as userfile:
+            return userfile.read()
+    except:
+        return None
+
+def create_user(id: int, name: str) -> str:
+    with open('data/users/' + str(id), 'w+') as userfile:
+        userfile.write(name)
+        return name
+
+def submit(id: int, start: int, end: int):
+    print(str(id) + ' / ' + str(start) + '-' + str(end))
