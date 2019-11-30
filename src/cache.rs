3151282a45e7ebb71d5efd76dca3680856381cd4
Leo Wilson <leo@leomwilson.com>
2019-11-30 14:13:43 -0700

    Add session code in cache

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,7 @@
 use directories::ProjectDirs;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::fs;
 
@@ -51,4 +52,36 @@ fn exists(fname: &str) -> bool {
     let mut fpath = cfg_dir();
     fpath.push(Path::new(fname));
     Path::new(&fpath.as_path()).exists()
+}
+
+fn read(fname: &str) -> String {
+    let mut fpath = cfg_dir();
+    fpath.push(Path::new(fname));
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&fpath.as_path())
+        .unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents);
+    contents
+}
+
+pub fn is_signed_in(id: &str) -> bool {
+    exists(&format!("sess/{}", id))
+}
+
+pub fn mk_sess(id: &str, start: u64) {
+    create(&format!("sess/{}", id), &format!("{}", start));
+}
+
+pub fn get_sess_start(id: &str) -> u64 {
+    read(&format!("sess/{}", id)).parse::<u64>().unwrap()
+}
+
+pub fn rm_and_get_sess(id: &str) -> u64 {
+    let start = get_sess_start(&id);
+    del(&format!("sess/{}", id));
+    start
 }
