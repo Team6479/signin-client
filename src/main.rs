e492efb2d37537ffb096cb061390701a463110db
Leo Wilson <leo@leomwilson.com>
2019-12-31 14:34:52 -0700

    Skeleton code for user creation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_must_use)]
 
 use cursive::Cursive;
+use cursive::traits::*;
 use cursive::views::{Dialog, TextView, EditView, DummyView, Button, LinearLayout};
 
 mod util;
@@ -14,6 +15,8 @@ fn main() {
     tui.add_layer(Dialog::around(LinearLayout::vertical()
             .child(Button::new("Sign In", signin_dialog))
             .child(Button::new("Sign Out", signout_dialog))
+            .child(DummyView)
+            .child(Button::new("Create User", newuser_dialog))
             .child(DummyView)
             .child(Button::new("Admin", |s| s.quit())))
         .title("Options"));
@@ -47,10 +50,13 @@ fn signin_dialog(s: &mut Cursive) {
                             }));
                     }
                 } else {
-                    s.add_layer(Dialog::around(TextView::new("Please use your student ID number."))
-                        .title("Invalid ID")
-                        .button("Ok", |s| {
+                    s.add_layer(Dialog::around(TextView::new("This user has not been created. Would you like to create one?"))
+                        .title("User does not exist")
+                        .button("No", |s| {
                             s.pop_layer();
+                        })
+                        .button("Yes", |s| {
+                            newuser_dialog(s);
                         }));
                 }
             }))
@@ -80,7 +86,7 @@ fn signout_dialog(s: &mut Cursive) {
                     } else {
                         s.pop_layer();
                         s.add_layer(Dialog::around(TextView::new(format!("Users cannot sign out before signing in. Sign in?")))
-                            .title("Net signed in")
+                            .title("Not signed in")
                             .button("No", |s| {
                                 s.pop_layer();
                             })
@@ -90,15 +96,74 @@ fn signout_dialog(s: &mut Cursive) {
                             }));
                     }
                 } else {
-                    s.add_layer(Dialog::around(TextView::new("Please use your student ID number."))
-                        .title("Invalid ID")
-                        .button("Ok", |s| {
+                    s.add_layer(Dialog::around(TextView::new("This user has not been created. Would you like to create one?"))
+                        .title("User does not exist")
+                        .button("No", |s| {
                             s.pop_layer();
+                        })
+                        .button("Yes", |s| {
+                            s.pop_layer();
+                            newuser_dialog(s);
+                            signin_dialog(s);
                         }));
                 }
             }))
         .title("Enter or scan your ID to sign out")
         .button("Cancel", |s| {
             s.pop_layer();
+        }));
+}
+
+fn newuser_dialog(s: &mut Cursive) {
+    s.add_layer(Dialog::around(LinearLayout::horizontal()
+        .child(LinearLayout::vertical()
+            .child(TextView::new("ID"))
+            .child(TextView::new("Name")))
+        .child(LinearLayout::vertical()
+            .child(EditView::new()
+                .with_id("id"))
+            .child(EditView::new()
+                .with_id("name"))))
+        .title("Enter your ID and name")
+        .button("Cancel", |s| {
+            s.pop_layer();
+        })
+        .button("Create", |s| {
+            let id = s.call_on_id("id", |view: &mut EditView| {
+                    view.get_content()
+                }).unwrap();
+            let name = s.call_on_id("name", |view: &mut EditView| {
+                    view.get_content()
+                }).unwrap();
+            
+            match user::is_creatable(user::User {
+                id: (&id).to_string(), // yes, I know
+                name: (&name).to_string(), // it works, ok?
+                lvl: 1, // all users are lvl 1 for now
+            }) {
+                user::Creatability::Unobstructed => {
+                    s.pop_layer();
+                    // TODO: create user
+                    s.add_layer(Dialog::around(TextView::new(format!("User \"{}\" with id {} has been created.\nDon't forget to sign in again if desired.", name, id)))
+                            .title("Successfully created user")
+                            .button("Ok", |s| {
+                                s.pop_layer();
+                            }));
+                },
+                user::Creatability::Privileged => {
+                    s.add_layer(Dialog::around(TextView::new("This doesn't look like a valid CdS ID.\nIf this is your ID, talk to an officer for help."))
+                            .title("Invalid format")
+                            .button("Ok", |s| {
+                                s.pop_layer();
+                            }));
+                },
+                user::Creatability::Impossible => {
+                    s.add_layer(Dialog::around(TextView::new("The format of your ID and/or name is not allowed.\nGenerally, this is due to commas.\nPlease fix and re-enter, or talk to an officer for help."))
+                            .title("Disallowed data")
+                            .button("Ok", |s| {
+                                s.pop_layer();
+                            }));
+                },
+            }
         }));
 }
